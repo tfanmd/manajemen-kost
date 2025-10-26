d336750b1986d2d3780514aba617ef2cb38d2723
tfanmd <ahmdtaufan122004@gmail.com>
2025-10-26 20:51:23 +0700

    First commit

---
+++ app/Models/BoardingHouse.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class BoardingHouse extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'city_id',
+        'category_id',
+        'description',
+        'price',
+        'address', 
+    ];
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
